303219bdc489119e1c73022f8b3aa11a404c50eb
Arturs Vonda <arturs.vonda@lokalise.com>
2023-02-24 12:27:20 +0200

    feat: adjust prettier config
    Use tabs instead of spaces
    Use trailing commas

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,29 +1,29 @@
 const Configuration = {
-  extends: ["@commitlint/config-conventional"],
-  rules: { "breaking-changes-rules": [2, "never"] },
-  parserPreset: {
-    parserOpts: { noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"] },
-  },
-  plugins: [
-    {
-      rules: {
-        "breaking-changes-rules": (parsed) => {
-          if (
-            ["chore", "refactor"].includes(parsed.type) &&
-            parsed.footer !== null &&
-            parsed.footer.includes("BREAKING CHANGE")
-          ) {
-            return [
-              false,
-              `${parsed.type} commits cannot contain breaking changes`,
-            ];
-          } else {
-            return [true];
-          }
-        },
-      },
-    },
-  ],
+	extends: ["@commitlint/config-conventional"],
+	rules: { "breaking-changes-rules": [2, "never"] },
+	parserPreset: {
+		parserOpts: { noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"] },
+	},
+	plugins: [
+		{
+			rules: {
+				"breaking-changes-rules": (parsed) => {
+					if (
+						["chore", "refactor"].includes(parsed.type) &&
+						parsed.footer !== null &&
+						parsed.footer.includes("BREAKING CHANGE")
+					) {
+						return [
+							false,
+							`${parsed.type} commits cannot contain breaking changes`,
+						];
+					} else {
+						return [true];
+					}
+				},
+			},
+		},
+	],
 };
 
 module.exports = Configuration;
