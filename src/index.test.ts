6bd1fba937716765c277faf5af7da9fae4c1b25e
Arturs Vonda <arturs.vonda@lokalise.com>
2023-02-24 12:27:20 +0200

    feat: adjust prettier config
    Use tabs instead of spaces
    Use trailing commas

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,7 @@
 import { add } from "./index";
 
 describe("add", () => {
-  it("can add two numbers", () => {
-    expect(add(1, 2)).toBe(3);
-  });
+	it("can add two numbers", () => {
+		expect(add(1, 2)).toBe(3);
+	});
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,14 @@ import { resolve } from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "YourProjectName",
-      fileName: "your-project-name",
-    },
-  },
-  test: {
-    globals: true,
-  },
+	build: {
+		lib: {
+			entry: resolve(__dirname, "src/index.ts"),
+			name: "YourProjectName",
+			fileName: "your-project-name",
+		},
+	},
+	test: {
+		globals: true,
+	},
 });
